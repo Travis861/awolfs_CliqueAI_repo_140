6d293be051f4475f681069ab649ce5a1a7a3c892
Travis861 <bittensor861@gmail.com>
2026-04-27 10:18:07 -0700

    miner_6

--- start_miner.sh
+++ start_miner.sh
@@ -8,7 +8,7 @@ cd "$PROJECT_ROOT"
 
 VENV_DIR="$PROJECT_ROOT/../.venv"
 if [ ! -d "$VENV_DIR" ]; then
-    python3 -m venv "$VENV_DIR"
+    python3 -m .venv "$VENV_DIR"
 fi
 
 source "$VENV_DIR/bin/activate"
